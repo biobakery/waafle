5f5faaa1afde4dd05d7a05909aa5e5a451d7853d
ljmciver <lauren.j.mciver@gmail.com>
2024-10-09 16:30:19 -0400

    Adding a small amount of additional, optional information to the setup.py for the PyPI package

--- setup.py
+++ setup.py
@@ -14,8 +14,16 @@ setuptools.setup(
     long_description_content_type = "text/markdown",
     url = "https://huttenhower.sph.harvard.edu/waafle",
     packages = setuptools.find_packages( ),
+    keywords=['microbial','microbiome','bioinformatics','microbiology','metagenomic','metatranscriptomic','waafle'],
+    platforms=['Linux','MacOS'],
     classifiers = [
         "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
     ],
     entry_points = {
         "console_scripts": [
